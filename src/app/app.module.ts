1525b2af6f54532ddd02991a6f3de8f152a6fe26
warolsky <kirinasta@gmail.com>
2020-09-10 14:30:33 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,47 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { BudgetComponent } from './components/budget/budget.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MenuNavComponent } from './components/menu-nav/menu-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {Routes} from '@angular/router';
+import { BudgetTableComponent } from './components/budget-table/budget-table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+
+const routes: Routes = [
+  {path: 'budget', component: BudgetComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BudgetComponent,
+    MenuNavComponent,
+    BudgetTableComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
